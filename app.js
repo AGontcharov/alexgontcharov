e8c943486f2dcedbdee6facc5b4e6c288f45d9f9
Alexander Gontcharov <gontchaa@mail.uoguelph.ca>
2017-02-23 20:01:45 -0500

    minor changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var app = angular.module('www/myApp', []);
+var app = angular.module('myApp', []);
 
-app.config(function($routeProvider)) {
+app.config(function($routeProvider) {
 	$routeProvider
 
 	// route for the home page
@@ -11,19 +11,19 @@ app.config(function($routeProvider)) {
 
 	// route for the page 1
 	.when('/page1', {
-		viewURL: 'view/page1/html',
+		viewURL: 'view/page1.html',
 		controller: 'page1Controller'
 	})
 
 	// route for the page 2
 	.when('/page2', {
-		viewURL: 'view/page2/html',
+		viewURL: 'view/page2.html',
 		controller: 'page2Controller'
 	})
 
 	// route for the page 3
 	.when('/page3', {
-		viewURL: 'view/page3/html',
+		viewURL: 'view/page3.html',
 		controller: 'page3Controller'
 	});
 
