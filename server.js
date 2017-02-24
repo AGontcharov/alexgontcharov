0635124b264732d655cf191a9958a06096d2e4d7
Alexander Gontcharov <gontchaa@mail.uoguelph.ca>
2017-02-23 21:24:59 -0500

    Local host working now

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+//set the root directory of the project, required for res.sendFile.
+app.use('/', express.static(__dirname + '/'));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+//app is now fully initialized, listen on port 3000 and await a request from the client.
+app.listen(3000, function() {
+ 	console.log("Now listening on 3000.");
+});
