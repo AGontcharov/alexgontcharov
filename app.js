64daf381e79b0adf06d25601f2c7aad512e4ded9
Alexander Gontcharov <gontchaa@mail.uoguelph.ca>
2017-02-23 19:05:38 -0500

    Updated directory structure and implemented routes

---
+++ app.js
@@ -0,0 +1,30 @@
+var app = angular.module('www/myApp', []);
+
+app.config(function($routeProvider)) {
+	$routeProvider
+
+	// route for the home page
+	.when('/', {
+		viewURL : 'view/home.html',
+		controller : 'mainController'
+	})
+
+	// route for the page 1
+	.when('/page1', {
+		viewURL: 'view/page1/html',
+		controller: 'page1Controller'
+	})
+
+	// route for the page 2
+	.when('/page2', {
+		viewURL: 'view/page2/html',
+		controller: 'page2Controller'
+	})
+
+	// route for the page 3
+	.when('/page3', {
+		viewURL: 'view/page3/html',
+		controller: 'page3Controller'
+	});
+
+});
