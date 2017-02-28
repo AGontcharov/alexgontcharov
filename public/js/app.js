a3b902b2b8d63962528784357fcd6e5b223d50c1
Alexander Gontcharov <gontchaa@mail.uoguelph.ca>
2017-02-28 01:58:29 -0500

    html5mode working

--- public/js/app.js
+++ public/js/app.js
@@ -1,36 +1,39 @@
 var myApp = angular.module('myApp', ['ngRoute']);
 
 myApp.config(function($routeProvider, $locationProvider) {
-	$locationProvider.hashPrefix('');
+	//$locationProvider.hashPrefix('');
 	$routeProvider
 
 	// route for the home page
 	.when('/', {
-		templateUrl: 'public/view/home.html',
+		templateUrl: '/view/home.html',
 		controller: 'mainController',
 	})
 
 	// route for the page 1
 	.when('/projects', {
-		templateUrl: 'public/view/projects.html',
+		templateUrl: '/view/projects.html',
 		controller: 'projectsController'
 	})
 
 	// route for the page 2
 	.when('/resume', {
-		templateUrl: 'public/view/resume.html',
+		templateUrl: '/view/resume.html',
 		controller: 'resumeController',
 	})
 
 	// route for the page 3
 	.when('/about', {
-		templateUrl: 'public/view/about.html',
+		templateUrl: '/view/about.html',
 		controller: 'page3Controller'
 	})
 
 	.otherwise({
 		redirectTo: '/404'
 	});
+
+	// use the HTML5 History API
+	$locationProvider.html5Mode(true);
 });
 
 myApp.controller('mainController', function($scope) {
