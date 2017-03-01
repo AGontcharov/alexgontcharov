6a8348d0aac7aac7444da70ef51acae73da2010d
Alexander Gontcharov <gontchaa@mail.uoguelph.ca>
2017-03-01 03:13:48 -0500

    Setting up reverse proxy server

--- server.js
+++ server.js
@@ -8,21 +8,6 @@ app.get('*', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-//Not what I want, just sends the html only.
-/*app.get('/projects', function (req, res) {
-	res.sendFile(__dirname + '/public/view/projects.html');
-});*/
-
-// This works only for projects though
-/*app.get('/projects', function (req, res) {
-	res.sendFile(__dirname + '/public/index.html');
-});*/
-
-// This is a server file
-/*app.get("*", function(req, res) {
-    res.render("./index.html");
-});*/
-
 //app is now fully initialized, listen on port 3000 and await a request from the client.
 app.listen(3000, function() {
  	console.log("Now listening on 3000.");
