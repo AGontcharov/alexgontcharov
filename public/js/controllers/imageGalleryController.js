cff4a4ec05de19711d3d239602e9de05c7567911
Alexander Gontcharov <gontchaa@mail.uoguelph.ca>
2017-06-16 22:32:18 -0400

    Testing vertical and horizontal alignment

--- public/js/controllers/imageGalleryController.js
+++ public/js/controllers/imageGalleryController.js
@@ -1,10 +1,31 @@
 var app = angular.module('myApp');
 
 app.controller('imageGalleryController', function($scope) {
-	$scope.openModal = function(slide) {
-		console.log(slide);
+	var imageIndex = 0;
 
-		
+	$scope.getIndex = function(index) {
+		console.log(index);
+		imageIndex = index - 1;
+		showSlide(imageIndex);
+	}
+
+	/* Display the image. */
+	function showSlide(n) {
+		var slides = document.getElementsByClassName("image-wrapper");
+
+		if (n > slides.length) {slideIndex = 0}
+ 		if (n < 1) {slideIndex = slides.length - 1}
+
+ 		/* Hide all the active images in the lightbox. */
+		for (i = 0; i < slides.length; i++) {
+			slides[i].style.display = "none";
+		}
+		slides[n].style.display = "block";
+	}
+
+	function plusSlide(n) {
+		console.log(n);
+		// showSlide(imageIndex += n);
 	}
 
 });
