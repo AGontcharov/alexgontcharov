ca5f475b4091a8bfd4693048f6d8526371a7ed3d
Alexander Gontcharov <gontchaa@mail.uoguelph.ca>
2017-06-19 00:13:23 -0400

    Added functionality to slide images in lightbox

--- public/js/controllers/imageGalleryController.js
+++ public/js/controllers/imageGalleryController.js
@@ -1,31 +1,30 @@
 var app = angular.module('myApp');
 
 app.controller('imageGalleryController', function($scope) {
-	var imageIndex = 0;
+	var slideIndex = 0;
 
-	$scope.getIndex = function(index) {
-		console.log(index);
-		imageIndex = index - 1;
-		showSlide(imageIndex);
+	$scope.getIndex = function(n) {
+		slideIndex = n;
+		showSlide(slideIndex);
 	}
 
 	/* Get the next or previous image. */
-	function plusSlide(n) {
-		console.log(n);
-		showSlide(imageIndex += n);
+	$scope.plusSlide = function(n) {
+		showSlide(slideIndex += n);
 	}
 
 	/* Display the image. */
 	function showSlide(n) {
 		var slides = document.getElementsByClassName("image-wrapper");
 
-		if (n > slides.length) slideIndex = 0;
- 		if (n < 1) slideIndex = slides.length - 1;
+		if (n > slides.length) slideIndex = 1;
+ 		if (n < 1) slideIndex = slides.length;
+		console.log('Index is ' + slideIndex);
 
  		/* Hide all the active images in the lightbox. */
 		for (i = 0; i < slides.length; i++) {
 			slides[i].style.display = "none";
 		}
-		slides[n].style.display = "block";
+		slides[slideIndex - 1].style.display = "block";
 	}
 });
