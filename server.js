914d115bd4234881587fb0b07ce418bd6ac80160
Alexander Gontcharov <gontchaa@mail.uoguelph.ca>
2017-09-06 22:54:54 -0400

    Updated resume

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var app = express();
 
-//set the root directory of the project, required for res.sendFile.
+// Sets the root directory of the project
 app.use('/', express.static(__dirname + '/public'));
 
 app.get('*', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-//app is now fully initialized, listen on port 3000 and await a request from the client.
 app.listen(3000, function() {
 	console.log("Now listening on 3000.");
 });
