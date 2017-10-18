7fc8dc2ab6de898deb660fd3117ddebcef0cb542
Alexander Gontcharov <gontchaa@mail.uoguelph.ca>
2017-10-18 11:54:23 -0400

    Added Qlik project

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ app.get('*', function (req, res) {
 
 // Create server and listen on port 3000
 app.listen(3000, function() {
-	console.log("Now listening on 3000.");
+  console.log("Now listening on 3000.");
 });
